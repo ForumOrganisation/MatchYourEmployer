4e1dd082c811170586b3f0a7177a5ab05e3addcb
El Alaoui El Abdellaoui <ghali.elabdellaoui@gmail.com>
2020-01-11 03:16:51 +0200

    Student and company classes attributes added

--- source/model/company.py
+++ source/model/company.py
@@ -2,8 +2,25 @@ import numpy.random as rd
 
 
 class Company:
-    def __init__(self):
-        self.id_company = rd.randint(pow(10, 9))  # TODO pick a better id
+
+    company_id_count = 1
+
+    def __init__(self, domains, accepted_nationalities, domains_localisation,
+                 school, presence_day, wanted_diploma, wanted_department,
+                 mobility, offer_length):
+        self.id_company = Company.company_id_count  # Same as Student class
+        Company.company_id_count += 1
+        self.domains_localisation = dict(domains_localisation)
+        self.domains = list(domains)
+        self.accepted_nationalities = list(accepted_nationalities)
+        self.school = school  # boolean
+        self.presence_day = presence_day  # Same as Student class
+        self.wanted_diploma = list(wanted_diploma)
+        self.wanted_department = list(wanted_department)
+        self.mobility = mobility  # Same as Student class
+        self.offer_length = offer_length  # 1 if they accept less than 6 months, 0 otherwise
+
+
 
     def __str__(self):
         return "Company " + str(self.id_company)
--- source/model/student.py
+++ source/model/student.py
@@ -2,8 +2,23 @@ import numpy.random as rd
 
 
 class Student:
-    def __init__(self):
-        self.id_student = rd.randint(pow(10, 9))  # TODO pick a better id
+
+    student_id_count = 1  # static variable counting the number of students
+
+    def __init__(self, diploma, school, centre_of_inter,
+                 offer_length, department, geo_mobility,
+                 nationality, presence_day, study_after_graduate):
+        self.id_student = Student.student_id_count  # a student's ID takes the value of the instance count variable
+        Student.student_id_count += 1
+        self.diploma = diploma
+        self.school = school
+        self.centre_of_inter = centre_of_inter
+        self.offer_length = offer_length
+        self.department = department
+        self.geo_mobility = geo_mobility # takes values fr, e, w for france, europe and world
+        self.nationality = nationality
+        self.presence_day = presence_day  # takes 3 values: w, t or b, for wednesday, thursday and both
+        self.study_after_graduate = study_after_graduate  # boolean
 
     def __str__(self):
         return "Student " + str(self.id_student)
