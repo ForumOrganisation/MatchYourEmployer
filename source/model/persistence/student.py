99941e77f0b2ac6106a1438cd3b3b43866318c80
Kenzananas <bouzidkenza1@gmail.com>
2020-02-06 14:34:00 +0100

    Geolocation working + selectZEC updated

---
+++ source/model/persistence/student.py
@@ -0,0 +1,47 @@
+# coding=utf-8
+
+from sqlalchemy import Column, String, Integer, Boolean, CHAR, ForeignKey, Table
+from sqlalchemy.orm import relationship
+
+from base import Base
+
+students_offers_locations_association = Table(
+    'students _offers_locations', Base.metadata,
+    Column('student_id', Integer, ForeignKey('students.id')),
+    Column('offers_locations_id', Integer, ForeignKey('offers_locations.id'))
+)
+
+students_sub_domains_association = Table(
+    'students_sub_domains', Base.metadata,
+    Column('student_id', Integer, ForeignKey('students.id')),
+    Column('sub_domain_id', Integer, ForeignKey('sub_domains.id'))
+)
+
+
+class Student(Base):
+    __tablename__ = 'students'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String)
+    school = Column(String)
+    main_domain = Column(String)
+    email = Column(String)
+    offer_length = Column(Integer)
+    contract_type = Column(String)
+    company_size = Column(String)
+    nationality = Column(String)
+    geo_mobility = relationship("main_domain", secondary=students_offers_locations_association)
+    sub_domains = relationship("main_domain", secondary=students_sub_domains_association)
+
+    def __init__(self, first_name, last_name, email, offer_length, contract_type,
+                 company_size, nationality):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.offer_length = offer_length
+        self.contract_type = contract_type
+        self.company_size = company_size
+        self.nationality = nationality
+
